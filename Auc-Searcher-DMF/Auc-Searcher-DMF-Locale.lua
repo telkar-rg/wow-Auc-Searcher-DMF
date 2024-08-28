6a090d8ada7f907445c9d3ae0e72f5c381c80f65
telkar-rg <telkar-rg@gmx.net>
2024-08-28 21:29:58 +0200

    completion of cleanup in DM tab

--- Auc-Searcher-DMF/Auc-Searcher-DMF-Locale.lua
+++ Auc-Searcher-DMF/Auc-Searcher-DMF-Locale.lua
@@ -7,15 +7,15 @@ local GAME_LOCALE = GetLocale()
 
 
 L["Searcher_Title"] = 	"Darkmoon"
-L["Searcher_Desc"] = 	"Search for Darkmoon cards & ingredients"
-L["Header_Title"] = 	"Darkmoon cards & ingredients search criteria"
+L["Searcher_Desc"] = 	"Search for Darkmoon Cards & Reagents"
+L["Header_Title"] = 	"Darkmoon Cards & Reagents search criteria"
 L["Header_Desc"] = 		"Scan: Quest, Trade Goods (Elemental, Herb, Parts, Other)"
 
-L["Money.deckprice"] = 		format("Budget per \124cffa335ee%s\124r* (350 rep)", "Epic Deck")
+L["Money.deckprice"] = 		format("Budget per \124cffaa47ee%s\124r* (350 rep)", "Epic Deck")
 L["Money.mats.eternal"] = 	format("Budget per \124cff1eff00%s\124r", "Eternal Life")
 L["Money.mats.primal"] = 	format("Budget per \124cff1eff00%s\124r", "Primal Life")
 
-L["Note.deckprice"] = 	"*Items in the Auction House will be weighted according to their share in the complete Epic Deck (350 rep) and are only shown, if they fit inside the Budget.\n\nIn case of the lesser Decks, this will be scaled to the equivalent of 350 rep."
+L["Note.deckprice"] = 	format("*Items in the Auction House will be weighted according to their share in the complete \124cffaa47ee%s\124r (350 rep) and are only shown, if they fit inside the Budget.\n\n", "Epic Deck") .. format("The Budget of \124cff1eff00%s\124r will be assumed as the cost when weighing Reagents for TBC/WotLK cards.\n\n", "Primal Life/Eternal Life") .. format("In case of the \124cffc43b52%s\124r, this will be scaled to the equivalent of 350 rep.", "lesser Decks")
 
 
 L["Checkbox.cards.wotlk"] = format("Cards/Decks: \124cff3FC7EB%s\124r", "WotLK")
@@ -26,31 +26,54 @@ L["Checkbox.cards.tbc"] =	format("Cards/Decks: \124cffAAD372%s\124r", "TBC")
 L["Checkbox.craft.tbc"] =	format("Reagents: \124cffAAD372%s\124r cards (325 Inscr.)", "TBC")
 L["Checkbox.mats.primal"] =	format("Include \124cff1eff00%s\124r", "Primal Life")
 
-L["Checkbox.cards.vanilla"] = format("Cards/Decks: \124cffFFF468%s\124r", "Vanilla")
-L["Checkbox.craft.vanilla"] = format("Reagents: \124cffFFF468%s\124r cards (275 Inscr.)", "Vanilla")
+L["Checkbox.cards.vanilla"] = format("Cards/Decks: \124cffFFF468%s\124r", "Classic")
+L["Checkbox.craft.vanilla"] = format("Reagents: \124cffFFF468%s\124r cards (275 Inscr.)", "Classic")
 
+L["Checkbox.cards.demon"] = format("Cards/Decks: \124cffC41E3A%s\124r", "Demons")
+L["Checkbox.craft.demon"] = format("Reagents: \124cffC41E3A%s\124r cards (225 Inscr.)", "Demons")
+
+L["Checkbox.cards.mage"] = format("Cards/Decks: \124cffc43b52%s\124r", "Mages")
+L["Checkbox.craft.mage"] = format("Reagents: \124cffc43b52%s\124r cards (175 Inscr.)", "Mages")
+
+L["Checkbox.cards.sword"] = format("Cards/Decks: \124cffc4586a%s\124r", "Swords")
+L["Checkbox.craft.sword"] = format("Reagents: \124cffc4586a%s\124r cards (125 Inscr.)", "Swords")
+
+L["Checkbox.cards.rogue"] = format("Cards/Decks: \124cffc47683%s\124r", "Rogues")
+L["Checkbox.craft.rogue"] = format("Reagents: \124cffc47683%s\124r cards (85 Inscr.)", "Rogues")
 
 
 if GAME_LOCALE == "deDE" then
 	L["Searcher_Title"] = 	"Dunkelmond"
-	L["Searcher_Desc"] = 	"Suche nach Dunkelmond-Karten & Zutaten"
-	L["Header_Title"] = 	"Dunkelmond-Karten & Zutaten - Suchkriterium"
+	L["Searcher_Desc"] = 	"Suche nach Dunkelmond-Karten & Reagenzien"
+	L["Header_Title"] = 	"Dunkelmond-Karten & Reagenzien - Suchkriterium"
 	L["Header_Desc"] = 		"Scanne: Quest, Handwerkswaren (Elementar, Kräuter, Teile, Sonstige)"
 
-	L["Note.deckprice"] = 	"*Items im Auktionshause werden entsprechend ihrem Anteil am fertigen Epic Kartenset (350 Ruf) gewichtet und nur dann aufgeführt, wenn sie in das Budget passen.\n\nIm Falle der kleinen Kartensets wird dies auf 350 Ruf hochgerechnet."
+	L["Note.deckprice"] = 	format("*Items im Auktionshause werden entsprechend ihrem Anteil am fertigen \124cffaa47ee%s\124r (350 Ruf) gewichtet und nur dann aufgeführt, wenn sie in das Budget passen.\n\n", "Epic Kartenset") .. format("Das Budget von \124cff1eff00%s\124r wird als Preis angenommen, wenn der Anteil der Reagenzien für TBC/WotLK Karten gewichtet wird.\n\n", "Urleben/Äonenleben") .. format("Im Falle der \124cffc43b52%s\124r wird dies auf 350 Ruf hochgerechnet.", "kleinen Kartensets")
 
-	L["Money.deckprice"] = 		format("Budget pro \124cffa335ee%s\124r* (350 Ruf)", "Epic Kartenset")
+	L["Money.deckprice"] = 		format("Budget pro \124cffaa47ee%s\124r* (350 Ruf)", "Epic Kartenset")
 	L["Money.mats.eternal"] = 	format("Budget pro \124cff1eff00%s\124r", "Äonenleben")
 	L["Money.mats.primal"] = 	format("Budget pro \124cff1eff00%s\124r", "Urleben")
 	
 	L["Checkbox.cards.wotlk"] = format("Karten/Sets: \124cff3FC7EB%s\124r", "WotLK")
-	L["Checkbox.craft.wotlk"] = format("Reagenzien: \124cff3FC7EB%s\124r Karten (400 IK)", "WotLK")
+	L["Checkbox.craft.wotlk"] = format("Reagenzien: \124cff3FC7EB%s\124r-Karten (400 IK)", "WotLK")
 	L["Checkbox.mats.eternal"] = format("\124cff1eff00%s\124r mitsuchen", "Äonenleben")
 
 	L["Checkbox.cards.tbc"] =	format("Karten/Sets: \124cffAAD372%s\124r", "TBC")
-	L["Checkbox.craft.tbc"] =	format("Reagenzien: \124cffAAD372%s\124r Karten (325 IK)", "TBC")
+	L["Checkbox.craft.tbc"] =	format("Reagenzien: \124cffAAD372%s\124r-Karten (325 IK)", "TBC")
 	L["Checkbox.mats.primal"] =	format("\124cff1eff00%s\124r mitsuchen", "Urleben")
 
-	L["Checkbox.cards.vanilla"] = format("Karten/Sets: \124cffFFF468%s\124r", "Vanilla")
-	L["Checkbox.craft.vanilla"] = format("Reagenzien: \124cffFFF468%s\124r Karten (275 IK)", "Vanilla")
+	L["Checkbox.cards.vanilla"] = format("Karten/Sets: \124cffFFF468%s\124r", "Classic")
+	L["Checkbox.craft.vanilla"] = format("Reagenzien: \124cffFFF468%s\124r-Karten (275 IK)", "Classic")
+
+	L["Checkbox.cards.demon"] = format("Karten/Sets: \124cffC41E3A%s\124r", "Dämonen")
+	L["Checkbox.craft.demon"] = format("Reagenzien: \124cffC41E3A%s\124r-Karten (225 IK)", "Dämonen")
+
+	L["Checkbox.cards.mage"] = format("Karten/Sets: \124cffc43b52%s\124r", "Magier")
+	L["Checkbox.craft.mage"] = format("Reagenzien: \124cffc43b52%s\124r-Karten (175 IK)", "Magier")
+
+	L["Checkbox.cards.sword"] = format("Karten/Sets: \124cffc4586a%s\124r", "Schwerter")
+	L["Checkbox.craft.sword"] = format("Reagenzien: \124cffc4586a%s\124r-Karten (125 IK)", "Schwerter")
+
+	L["Checkbox.cards.rogue"] = format("Karten/Sets: \124cffc47683%s\124r", "Schurken")
+	L["Checkbox.craft.rogue"] = format("Reagenzien: \124cffc47683%s\124r-Karten (85 IK)", "Schurken")
 end
--- Auc-Searcher-DMF/Auc-Searcher-DMF.lua
+++ Auc-Searcher-DMF/Auc-Searcher-DMF.lua
@@ -14,7 +14,7 @@ if not L then
 end
 
 -- Set our defaults
-default("darkmoonfairecards.deckprice", 5000*10000)
+default("darkmoonfairecards.deckprice", 2000*10000)
 
 default("darkmoonfairecards.cards.rogue", 1)
 default("darkmoonfairecards.cards.sword", 1)
@@ -39,7 +39,7 @@ default("darkmoonfairecards.overhead.eternal", 30*10000)
 default("darkmoonfairecards.mats.primal", 1)
 default("darkmoonfairecards.mats.eternal", 1)
 
-do
+
 function private.createMap(x)
 
 	local out = {};
@@ -52,6 +52,40 @@ function private.createMap(x)
 
 	return out;
 end
+
+local epicCardsDecksVanilla = private.createMap({
+	19227, 19230, 19231, 19232, 19233, 19234, 19235, 19236, -- beasts
+	19276, 19278, 19279, 19280, 19281, 19282, 19283, 19284, -- portals
+	19258, 19259, 19260, 19261, 19262, 19263, 19264, 19265, -- warlords
+	19268, 19269, 19270, 19271, 19272, 19273, 19274, 19275, -- elementals
+	19228, -- beasts
+	19277, -- portals
+	19257, -- warlords
+	19267, -- elementals
+
+0});
+
+local epicCardsDecksTbc = private.createMap({
+	31901, 31909, 31908, 31904, 31903, 31906, 31905, 31902, -- furies
+	31892, 31900, 31899, 31895, 31894, 31898, 31896, 31893, -- storms
+	31882, 31889, 31888, 31885, 31884, 31887, 31886, 31883, -- blessings
+	31910, 31918, 31917, 31913, 31912, 31916, 31915, 31911, -- lunacy
+	31907, -- furies
+	31891, -- storms
+	31890, -- blessings
+	31914, -- lunacy
+0});
+
+local epicCardsDecksWotlk = private.createMap({
+	44277, 44278, 44279, 44280, 44281, 44282, 44284, 44285, -- chaos
+	44268, 44269, 44270, 44271, 44272, 44273, 44274, 44275, -- nobles
+	44260, 44261, 44262, 44263, 44264, 44265, 44266, 44267, -- prisms
+	44286, 44287, 44288, 44289, 44290, 44291, 44292, 44293, -- undeath
+	44276, -- chaos
+	44326, -- nobles
+	44259, -- prisms
+	44294, -- undeath
+0});
 
 local epicCards = private.createMap({
 	19227, 19230, 19231, 19232, 19233, 19234, 19235, 19236, -- beasts
@@ -97,9 +131,33 @@ local reg4Cards = private.createMap({
 	37145, 37147, 37159, 37160, -- swords
 0});
 
+local reg5CardsMages = private.createMap({
+	44165, 44144, 44145, 44146, 44147, -- mages
+0});
+
+local reg5CardsDemons = private.createMap({
+	44143, 44154, 44155, 44156, 44157, -- demons
+0});
+
 local reg5Cards = private.createMap({
 	44165, 44144, 44145, 44146, 44147, -- mages
 	44143, 44154, 44155, 44156, 44157, -- demons
+0});
+
+local reg3Decks = private.createMap({
+	37163, 43039, -- rogues
+0});
+
+local reg4Decks = private.createMap({
+	37164, 42922, -- swords
+0});
+
+local reg5DecksMages = private.createMap({
+	44148, 44184, -- mages
+0});
+
+local reg5DecksDemons = private.createMap({
+	44158, 44185, -- demons
 0});
 
 local regDecks = private.createMap({
@@ -122,9 +180,14 @@ local rogue25Herbs = private.createMap({
 	2452, -- Swiftthistle
 0});
 
-local rogueInks = private.createMap({
+local rogueInksX2 = private.createMap({ -- X2
+	39334, -- Anthrazitfarbene Pigmente
+0});
+
+local rogueInks = private.createMap({ 	-- X1
 	43103, -- Verdant Pigment
 	43115, -- Hunter's Ink
+	39774, -- Mitternachtstinte
 0});
 
 -- **************************************************
@@ -139,9 +202,14 @@ local sword25Herbs = private.createMap({
 	3355, -- Wild Steelbloom
 0});
 
-local swordInks = private.createMap({
+local swordInksX1 = private.createMap({ -- X1
+	43116, -- Löwentinte
+0});
+
+local swordInks = private.createMap({ 	-- X2
 	43104, -- Burnt Pigment
 	43117, -- Dawnstar Ink
+	39338, -- Goldfarbene Pigmente
 0});
 
 -- **************************************************
@@ -156,9 +224,14 @@ local mage25Herbs = private.createMap({
 	3821, -- Goldthorn
 0});
 
+local mageInksX1 = private.createMap({
+	43118, -- Jadefeuertinte
+0});
+
 local mageInks = private.createMap({
 	43105, -- Indigo Pigment
 	43119, -- Royal Ink
+	39339, -- Smaragdfarbene Pigmente
 0});
 
 -- **************************************************
@@ -176,9 +249,14 @@ local demon25Herbs = private.createMap({
 	8838, -- Sungrass
 0});
 
+local demonInksX1 = private.createMap({
+	43120, -- Firmamenttinte
+0});
+
 local demonInks = private.createMap({
 	43106, -- Ruby Pigment
 	43121, -- Fiery Ink
+	39340, -- Violette Pigmente
 0});
 
 -- **************************************************
@@ -194,7 +272,15 @@ local vanilla25Herbs = private.createMap({
 	13464, -- Golden Sansam
 0});
 
-local vanillaInks = private.createMap({
+local vanillaInksX2 = private.createMap({ -- x2
+	43122, -- Perlmutttinte
+0});
+
+local vanillaInksX4 = private.createMap({ -- x5
+	39341, -- Silbrige Pigmente
+0});
+
+local vanillaInks = private.createMap({ -- x5
 	43107, -- Sapphire Pigment
 	43123, -- Ink of the Sky
 0});
@@ -213,6 +299,10 @@ local tbc25Herbs = private.createMap({
 	22785, -- Felweed
 	22789, -- Terocone
 	22787, -- Ragveil
+0});
+
+local wotlkPigments = private.createMap({
+	43109, -- Icy Pigment
 0});
 
 local tbcInks = private.createMap({
@@ -244,7 +334,7 @@ local wotlkPigments = private.createMap({
 local wotlkInks = private.createMap({
 	43127, -- Snowfall Ink
 0});
-end
+
 -- **************************************************
 
 function lib:MakeGuiConfig(gui)
@@ -278,20 +368,20 @@ function lib:MakeGuiConfig(gui)
 	gui:GetLast(id).clearance = 10;
 	local last_vanilla = gui:GetLast(id)
 	
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.demon", 	"Cards/Decks: Demons");
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.demon", 	"Reagents: Demons cards (225 Inscr.)"); -- lvl 20
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.demon", 	L["Checkbox.cards.demon"]);
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.demon", 	L["Checkbox.craft.demon"]); -- lvl 20
 	gui:GetLast(id).clearance = 5;
 	
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.mage", 		"Cards/Decks: Mages");
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.mage", 		"Reagents: Mages cards (175 Inscr.)"); -- lvl 20
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.mage", 	L["Checkbox.cards.mage"]);
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.mage", 	L["Checkbox.craft.mage"]); -- lvl 20
 	gui:GetLast(id).clearance = 5;
 	
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.sword", 	"Cards/Decks: Swords");
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.sword", 	"Reagents: Swords cards (125 Inscr.)"); -- lvl 10
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.sword", 	L["Checkbox.cards.sword"]);
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.sword", 	L["Checkbox.craft.sword"]); -- lvl 10
 	gui:GetLast(id).clearance = 5;
 	
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.rogue", 	"Cards/Decks: Rogues");
-	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.rogue", 	"Reagents: Rogues cards (85 Inscr.)"); -- lvl 10
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.cards.rogue", 	L["Checkbox.cards.rogue"]);
+	gui:AddControl(id, "Checkbox",          colPos, 0, "darkmoonfairecards.craft.rogue", 	L["Checkbox.craft.rogue"]); -- lvl 10
 	gui:GetLast(id).clearance = 5;
 	
 	-- **************************************************
@@ -303,14 +393,25 @@ function lib:MakeGuiConfig(gui)
 	gui:AddControl(id, "MoneyFramePinned",  colPos, 0, "darkmoonfairecards.overhead.primal", 1, 99999999, L["Money.mats.primal"]);
 	gui:GetLast(id).clearance = 5;
 									-- level, width, height, text
-	gui:AddControl(id, "Note",  colPos, 0, 280, 80, L["Note.deckprice"])
+	gui:AddControl(id, "Note",  colPos, 0, 280, 150, L["Note.deckprice"])
 	
 end
 
 function lib.Search(item)
 
 	local itemid = item[Const.ITEMID];
-
+	
+	-- Stop if new dont want to search Cards/Decks
+	if not(get("darkmoonfairecards.cards.wotlk")) 	and epicCardsDecksWotlk[itemid] then 	return false, "nope"; end 	-- Wotlk
+	if not(get("darkmoonfairecards.cards.tbc")) 	and epicCardsDecksTbc[itemid] 	then 	return false, "nope"; end 	-- tbc
+	if not(get("darkmoonfairecards.cards.vanilla")) and epicCardsDecksVanilla[itemid] then 	return false, "nope"; end 	-- vanilla
+	
+	if not(get("darkmoonfairecards.cards.demon")) 	and (reg5CardsDemons[itemid] or reg5DecksDemons[itemid]) then 	return false, "nope"; end 	-- demon
+	if not(get("darkmoonfairecards.cards.mage")) 	and (reg5CardsMages[itemid] or reg5DecksMages[itemid]) then 	return false, "nope"; end 	-- mage
+	if not(get("darkmoonfairecards.cards.sword")) 	and (reg4Cards[itemid] or reg4Decks[itemid]) then 	return false, "nope"; end 	-- sword
+	if not(get("darkmoonfairecards.cards.rogue")) 	and (reg3Cards[itemid] or reg3Decks[itemid]) then 	return false, "nope"; end 	-- rogue
+	
+	
 	-- get cost
 	local buyprice = item[Const.BUYOUT];
 	local stacksize = item[Const.COUNT];
@@ -345,7 +446,8 @@ function lib.Search(item)
 	if (get("darkmoonfairecards.craft.rogue")) then
 		if (rogue50Herbs[itemid]) then per_deck = 14 * 3 * 10; overhead = 14 * 3 * 15; end
 		if (rogue25Herbs[itemid]) then per_deck = 14 * 3 * 20; overhead = 14 * 3 * 15; end
-		if (rogueInks[itemid]   ) then per_deck = 14 * 3;      overhead = 14 * 3 * 15; end
+		if (rogueInksX2[itemid] ) then per_deck = 14 * 3 * 2;  overhead = 14 * 3 * 15; end
+		if (rogueInks[itemid]   ) then per_deck = 14 * 3 * 1;  overhead = 14 * 3 * 15; end
 	end
 
 	-- sword crafted
@@ -358,6 +460,7 @@ function lib.Search(item)
 		if (sword50Herbs[itemid]) then per_deck = 14 * 4 * 10 * 2; overhead = 14 * 4 * 125; end
 		if (sword25Herbs[itemid]) then per_deck = 14 * 4 * 20 * 2; overhead = 14 * 4 * 125; end
 		if (swordInks[itemid]   ) then per_deck = 14 * 4 * 2;      overhead = 14 * 4 * 125; end
+		if (swordInksX1[itemid] ) then per_deck = 14 * 4 * 1;      overhead = 14 * 4 * 125; end
 	end
 
 	-- mage crafted
@@ -370,6 +473,7 @@ function lib.Search(item)
 		if (mage50Herbs[itemid]) then per_deck = 14 * 5 * 10 * 2; overhead = 14 * 5 * 125; end
 		if (mage25Herbs[itemid]) then per_deck = 14 * 5 * 20 * 2; overhead = 14 * 5 * 125; end
 		if (mageInks[itemid]   ) then per_deck = 14 * 5 * 2;      overhead = 14 * 5 * 125; end
+		if (mageInksX1[itemid] ) then per_deck = 14 * 5 * 1;      overhead = 14 * 5 * 125; end
 	end
 
 
@@ -383,6 +487,7 @@ function lib.Search(item)
 		if (demon50Herbs[itemid]) then per_deck = 14 * 5 * 10 * 2; overhead = 14 * 5 * 1250; end
 		if (demon25Herbs[itemid]) then per_deck = 14 * 5 * 20 * 2; overhead = 14 * 5 * 1250; end
 		if (demonInks[itemid]   ) then per_deck = 14 * 5 * 2;      overhead = 14 * 5 * 1250; end
+		if (demonInksX1[itemid] ) then per_deck = 14 * 5 * 1;      overhead = 14 * 5 * 1250; end
 	end
 
 	-- vanilla crafted
@@ -394,6 +499,8 @@ function lib.Search(item)
 		if (vanilla50Herbs[itemid]) then per_deck = 8 * 10 * 5; overhead = 8 * 1250; end
 		if (vanilla25Herbs[itemid]) then per_deck = 8 * 20 * 5; overhead = 8 * 1250; end
 		if (vanillaInks[itemid]   ) then per_deck = 8 * 5;      overhead = 8 * 1250; end
+		if (vanillaInksX4[itemid] ) then per_deck = 8 * 4;      overhead = 8 * 1250; end
+		if (vanillaInksX2[itemid] ) then per_deck = 8 * 2;      overhead = 8 * 1250; end
 	end
 
 	-- BC crafted
@@ -420,11 +527,11 @@ function lib.Search(item)
 		if (wotlkInks[itemid]    ) then per_deck = 8 * 6;          overhead = 8 * (5000 + (3 * get("darkmoonfairecards.overhead.eternal"))); end
 	end
 
-
 	if (per_deck > 0) then
-		if (priceper <= (limit - overhead) / per_deck) then
-			local factor = priceper / ((limit - overhead) / per_deck);
-			return string.format("%2d%%", 100 * factor);
+		local costAllowed = (limit - overhead) / per_deck
+		if priceper <= costAllowed then
+			local factor = priceper / costAllowed;
+			return string.format("%3d%%", 100 * factor);
 		end
 	end
 
@@ -445,7 +552,7 @@ function lib.Search(item)
 		if ((itemid == 37704) and (priceper*10 <= p2)) then life_percent = (priceper * 10) / p2; end
 	end
 	if (life_percent > 0) then
-		return string.format("%2d%%", 100 * life_percent);
+		return string.format("%3d%%", 100 * life_percent);
 	end
 
 
